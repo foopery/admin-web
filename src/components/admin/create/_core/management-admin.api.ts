cc8790e52dc52069f923d4c86e283c7063d53a92
shimjaedoo <woen1637@naver.com>
2024-11-12 23:30:24 +0900

    [Feat] : Admin Update API Add

--- src/components/admin/create/_core/management-admin.api.ts
+++ src/components/admin/create/_core/management-admin.api.ts
@@ -1,10 +1,17 @@
-import { ICreateAdmin } from "../_interface/admin.create.interface";
+import { ICreateAdmin, IUpdateAdmin } from "../../_interface/admin.create.interface";
 import { apiInstance } from "../../../../_common/api.instance";
+import { IApiUpdateResponseAdmin } from "../../_interface/admin-list.interface";
 
 export const managementAdminApi = {
   create: async function (adminData: ICreateAdmin) {
     const { data } = await apiInstance.post("/management/admins", adminData);
-    console.log("Admin API Data", data);
+    // console.log("Admin API Data", data);
+    return data;
+  },
+
+  update: async function (adminData: IUpdateAdmin, id: number) {
+    const { data } = await apiInstance.put<IApiUpdateResponseAdmin>(`/management/admins/${id}`, adminData);
+    // console.log("Admin PUT API Data", data);
     return data;
   },
 };
