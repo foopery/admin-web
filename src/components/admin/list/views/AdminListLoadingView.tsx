4ea7a9249c64a7f8ca1162d3930ac0e488441fa6
shimjaedoo <woen1637@naver.com>
2024-11-12 23:32:24 +0900

    [Feat] : Admin Loading View Component

---
+++ src/components/admin/list/views/AdminListLoadingView.tsx
@@ -0,0 +1,9 @@
+import Body from "../../../../_common/Body";
+
+export default function AdminListLoadingView() {
+  return (
+    <Body title={"admin"} description={"Loading..."}>
+      <div>Loading...</div>
+    </Body>
+  );
+}
