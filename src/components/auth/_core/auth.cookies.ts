afd83ee97250691f8a3b859253197d5d6afa154f
shimjaedoo <woen1637@naver.com>
2024-11-12 23:31:38 +0900

    [Feat] : 쿠키 조회추가

--- src/components/auth/_core/auth.cookies.ts
+++ src/components/auth/_core/auth.cookies.ts
@@ -6,4 +6,7 @@ export const selectCookie = {
   setCookie: function ({ name, value, option }: { name: string; value: string; option: any }) {
     cookies.set(name, value, option);
   },
+  getCookie: function (name: string) {
+    return cookies.get(name);
+  },
 };
