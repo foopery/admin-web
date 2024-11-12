df60eed5af4aa64ab805f837b1de3cd25e501fbe
shimjaedoo <woen1637@naver.com>
2024-11-12 23:30:09 +0900

    [Feat] : Admin Update Query Add

--- src/components/admin/create/_core/management-admin.query.ts
+++ src/components/admin/create/_core/management-admin.query.ts
@@ -1,17 +1,31 @@
-import { useMutation } from "@tanstack/react-query";
-import { ICreateAdmin } from "../_interface/admin.create.interface";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { ICreateAdmin, IUpdateAdmin } from "../../_interface/admin.create.interface";
 import { managementAdminApi } from "./management-admin.api";
 
 export const managementAdminQuery = {
   useCreate: function () {
     return useMutation({
       mutationFn: (data: ICreateAdmin) => managementAdminApi.create(data),
+      mutationKey: ["AdminCreate"],
       onSuccess: (v) => {
         // console.log("Query Success", v.message);
         alert(v.message);
       },
       onError: (error: any) => {
         console.log("Query Error", error.response.data.message);
+      },
+    });
+  },
+
+  useUpdate: function () {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: ({ data, id }: { data: IUpdateAdmin; id: number }) => managementAdminApi.update(data, id),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ["AdminList"] });
+      },
+      onError: (error: any) => {
+        console.log("Query PUT Error", error);
       },
     });
   },
