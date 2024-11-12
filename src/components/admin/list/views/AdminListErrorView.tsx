f06a8877eb78366c2646697c5e8534c2e34ec2ea
shimjaedoo <woen1637@naver.com>
2024-11-12 23:32:31 +0900

    [Feat] : Admin Error View Component

---
+++ src/components/admin/list/views/AdminListErrorView.tsx
@@ -0,0 +1,9 @@
+import Body from "../../../../_common/Body";
+
+export default function AdminListErrorView() {
+  return (
+    <Body title={"admin"} description={"Error::"}>
+      <div>Error :: Back Page</div>
+    </Body>
+  );
+}
