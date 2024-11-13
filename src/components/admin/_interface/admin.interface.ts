865565c43491f139d671f2e0cd3280743fb870cb
shimjaedoo <woen1637@naver.com>
2024-11-13 15:11:18 +0900

    [Comment] : 각 인터페이스 주석추가

--- src/components/admin/_interface/admin.interface.ts
+++ src/components/admin/_interface/admin.interface.ts
@@ -15,8 +15,10 @@ export interface IAdmin {
   deletedAt: string | null; // 삭제일
 }
 
-/**  ------------------>    */
-
+/**
+ * 관리자 생성인터페이스
+ *
+ * */
 export interface ICreateAdmin {
   status: AdminStatus; // 관리자 상태
   role: AdminRole; // 관리자 권한
@@ -27,11 +29,15 @@ export interface ICreateAdmin {
   profileImageUrl?: string; // 프로필 이미지
 }
 
+/**
+ * 관리자 수정인터페이스
+ *
+ * */
 export interface IUpdateAdmin {
-  status?: AdminStatus;
-  role?: AdminRole;
-  loginId?: string;
-  name?: string;
-  birthDate?: string;
-  profileImageUrl?: string;
+  status?: AdminStatus; // 관리자 상태
+  role?: AdminRole; // 관리자 권한
+  loginId?: string; // 로그인 아이디
+  name?: string; // 이름
+  birthDate?: string; // 생년월일
+  profileImageUrl?: string; // 프로필 이미지
 }
