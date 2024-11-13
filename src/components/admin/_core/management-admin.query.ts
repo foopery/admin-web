22daa0e33375b6998d0fca025ca47350c7a33c01
shimjaedoo <woen1637@naver.com>
2024-11-13 15:07:26 +0900

    [Refactor] : Admin Query 리팩토링

--- src/components/admin/_core/management-admin.query.ts
+++ src/components/admin/_core/management-admin.query.ts
@@ -1,12 +1,13 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { ICreateAdmin, IUpdateAdmin } from "../../_interface/admin.create.interface";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { ICreateAdmin, IUpdateAdmin } from "../_interface/admin.interface";
 import { managementAdminApi } from "./management-admin.api";
+import { queryClient } from "../../../App";
 
 export const managementAdminQuery = {
+  /* 관리자 생성 */
   useCreate: function () {
     return useMutation({
       mutationFn: (data: ICreateAdmin) => managementAdminApi.create(data),
-      mutationKey: ["AdminCreate"],
       onSuccess: (v) => {
         // console.log("Query Success", v.message);
         alert(v.message);
@@ -16,16 +17,23 @@ export const managementAdminQuery = {
       },
     });
   },
-
+  /* 관리자 목록조회 */
+  useGets: function (page: string) {
+    return useQuery({
+      queryFn: () => managementAdminApi.gets(page),
+      queryKey: ["AdminList", page],
+    });
+  },
+  /* 관리자 수정 */
   useUpdate: function () {
-    const queryClient = useQueryClient();
     return useMutation({
       mutationFn: ({ data, id }: { data: IUpdateAdmin; id: number }) => managementAdminApi.update(data, id),
       onSuccess: () => {
         queryClient.invalidateQueries({ queryKey: ["AdminList"] });
       },
       onError: (error: any) => {
-        console.log("Query PUT Error", error);
+        console.log("Query Error", error.response.data.message);
+        alert(error.response.data.message);
       },
     });
   },
