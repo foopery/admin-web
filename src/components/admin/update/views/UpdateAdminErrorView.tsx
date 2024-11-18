3ab86bcb797a619793e2c686761a75698e3f10c3
shimjaedoo <woen1637@naver.com>
2024-11-19 00:21:19 +0900

    [Feat] 관리자 수정 에러뷰 컴포넌트

---
+++ src/components/admin/update/views/UpdateAdminErrorView.tsx
@@ -0,0 +1,11 @@
+import Body from "../../../../_common/Body";
+
+function UpdateAdminErrorView() {
+  return (
+    <Body title={"error"} description={"Error"}>
+      <></>
+    </Body>
+  );
+}
+
+export default UpdateAdminErrorView;
