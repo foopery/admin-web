aac9dde01c4fa60cf25449de8110c4516fbdb29e
shimjaedoo <woen1637@naver.com>
2024-11-19 00:21:35 +0900

    [Feat] 관리자 수정 로딩뷰 컴포넌트

---
+++ src/components/admin/update/views/UpdateAdminLoadingView.tsx
@@ -0,0 +1,9 @@
+import Body from "../../../../_common/Body";
+
+export default function UpdateAdminLoadingView() {
+  return (
+    <Body title={"Loading"} description={"Loading..."}>
+      <></>
+    </Body>
+  );
+}
