2e6df01b87494b2f61521f26e109f04a5bb8cba7
shimjaedoo <woen1637@naver.com>
2024-11-19 00:05:49 +0900

    [Chore] import 제거

--- src/components/auth/_views/AuthView.tsx
+++ src/components/auth/_views/AuthView.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { IoLockClosed, IoPerson } from "react-icons/io5";
 import { useForm } from "react-hook-form";
 import { inputValidator } from "../../../_functions/loginForm-validator";
