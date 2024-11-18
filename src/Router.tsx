f64f57de898a152028e405d9f89d2c655675d41e
shimjaedoo <woen1637@naver.com>
2024-11-19 00:19:50 +0900

    [Comment] router별 해당주석 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -11,6 +11,8 @@ import ContainerDetail from "./components/container/detail/ContainerDetail";
 import Issue from "./components/issue/Issue";
 import IssueDetail from "./components/issue/detail/IssueDetail";
 import CreateAdmin from "./components/admin/create/CreateAdmin";
+import AdminGet from "./components/admin/get/AdminGet";
+import UpdateAdmin from "./components/admin/update/UpdateAdmin";
 
 export default function Router() {
   const routers: { path: string; element: React.ReactNode; key: string }[] = [
@@ -20,54 +22,64 @@ export default function Router() {
       key: "auth",
     },
     {
-      path: "/",
+      path: "/", // 메인 대시보드
       element: <MainDashBoard />,
       key: "dashboard",
     },
     {
-      path: "/container",
+      path: "/container", // 컨테이너
       element: <Container />,
       key: "container",
     },
     {
-      path: "/admin",
+      path: "/admin", // 관리자 목록페이지
       element: <Admin />,
       key: "/admin",
     },
     {
-      path: "/comment",
+      path: "/comment", // 댓글 페이지
       element: <Comment />,
       key: "comment",
     },
     {
-      path: "/editor",
+      path: "/editor", // 코드 에디터
       element: <Editor />,
       key: "editor",
     },
     {
-      path: "/member",
+      path: "/member", // 멤버 페이지
       element: <Member />,
       key: "member",
     },
     {
-      path: "/container/:id",
+      path: "/container/:id", // 컨테이너 상세페이지
       element: <ContainerDetail />,
       key: "container-detail",
     },
     {
-      path: "/issue",
+      path: "/issue", // 이슈 페이지
       element: <Issue />,
       key: "issue",
     },
     {
-      path: "/issue/:id",
+      path: "/issue/:id", // 이슈 상세페이지
       element: <IssueDetail />,
       key: "issue-detail",
     },
     {
-      path: "/admin/management/create",
+      path: "/admin/management/write", // 관리자 생성페이지
       element: <CreateAdmin />,
-      key: "/admin/management/create",
+      key: "/admin/management/write",
+    },
+    {
+      path: "/admin/management/:id", // 관리자 상세페이지
+      element: <AdminGet />,
+      key: "/admin/management/:id",
+    },
+    {
+      path: "/admin/management/:id/modify", // 관리자 수정페이지
+      element: <UpdateAdmin />,
+      key: "/admin/management/:id/modify",
     },
   ];
   return (
