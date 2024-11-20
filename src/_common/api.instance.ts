87332fe60b42d171e2893367c22ae7a196b6ec8d
shimjaedoo <woen1637@naver.com>
2024-11-20 16:12:45 +0900

    [Feat] axios Interceptor 추가

--- src/_common/api.instance.ts
+++ src/_common/api.instance.ts
@@ -1,3 +1,24 @@
 import axios from "axios";
+import { selectCookie } from "../components/auth/_core/auth.cookies";
 
+/* API Instance */
 export const apiInstance = axios.create({ baseURL: "https://api-dev.foopery.com" });
+/* API Interceptor */
+apiInstance.interceptors.request.use(
+  (config) => {
+    const token = selectCookie.getCookie("Token");
+    console.log("Token:", token);
+    // config.headers['Content-Type'] = 'application/json';
+    // config.headers['Authorization'] = `Bearer ${token}`;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+      // config.headers["Content-Type"] = "application/json";
+    }
+    console.log("Request Headers:", config.headers);
+    return config;
+  },
+  (error) => {
+    console.log("Request Error:", error);
+    return Promise.reject(error);
+  },
+);
