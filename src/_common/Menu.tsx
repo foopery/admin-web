5f475c0b696fcad1f18e64c30253470ee950d2da
shimjaedoo <woen1637@naver.com>
2024-11-20 16:13:52 +0900

    [Feat] 프로필 아이콘추가

--- src/_common/Menu.tsx
+++ src/_common/Menu.tsx
@@ -2,10 +2,10 @@
 
 import React, { ReactElement } from "react";
 import { IconType } from "react-icons";
-import { FaAddressBook, FaCommentAlt, FaDocker } from "react-icons/fa";
+import { FaAddressBook, FaCommentAlt, FaDocker, FaUserCircle } from "react-icons/fa";
 import { MdNightlight } from "react-icons/md";
 import { LuLogOut } from "react-icons/lu";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import { IoBackspace, IoMenuOutline } from "react-icons/io5";
 import { RiAdminFill } from "react-icons/ri";
 import { PiCodeFill } from "react-icons/pi";
@@ -20,7 +20,9 @@ interface IMenuItem {
 export function Menu({ setToggle, toggle }: { setToggle: React.Dispatch<React.SetStateAction<boolean>>; toggle: boolean }) {
   const location = useLocation();
   const path = location.pathname;
-
+  const param = useParams();
+  const id = param.id;
+  console.log(id);
   const MenuItems: IMenuItem[] = [
     {
       path: "/container",
@@ -56,20 +58,42 @@ export function Menu({ setToggle, toggle }: { setToggle: React.Dispatch<React.Se
   return (
     <>
       {/** 사이드 바 */}
-      <div className={`flex flex-col lg:shadow-md lg:bg-white lg:gap-10 lg:justify-between lg:p-5 lg:w-80 sm:w-full sm:fixed sm:top-0 sm:left-0 sm:right-0 `}>
+      <div className={`flex flex-col lg:shadow-md lg:bg-white lg:gap-6 lg:justify-between lg:p-5 lg:w-80 sm:w-full sm:fixed sm:top-0 sm:left-0 sm:right-0 `}>
         {/** 사이드 바 타이틀 영역 */}
-        <div className={`flex lg:flex-col lg:justify-center lg:items-center lg:text-gray-700 sm:justify-between sm:px-2 sm:bg-gray-50 sm:h-16 sm:shadow-md sm:z-10`}>
+        <div className={`flex lg:flex-col lg:justify-center  lg:items-center lg:text-gray-700 sm:justify-between sm:px-2 sm:bg-gray-50 sm:h-16 sm:shadow-md sm:z-10`}>
           <img src={`/logo-d.png`} className={`object-contain lg:w-44 sm:w-10`} />
           <div className={`lg:pt-3 sm:flex sm:flex-col sm:justify-center sm:items-center`}>
             <p className={`lg:text-2xl sm:text-lg tracking-[0.4rem] uppercase font-bold`}>foopery</p>
             <p className={`sm:text-sm text-gray-300 tracking-[0.1rem]`}>Management App</p>
           </div>
+
           <div className={`lg:hidden sm:flex sm:items-center`}>
             {toggle ? (
               <IoBackspace className={`transition duration-300 text-3xl text-gray-700 sm:active:text-gray-400 `} onClick={() => setToggle(!toggle)} />
             ) : (
               <IoMenuOutline className={`transition duration-300 text-3xl text-gray-700 sm:active:text-gray-400 `} onClick={() => setToggle(!toggle)} />
             )}
+          </div>
+        </div>
+        {/* 버튼 */}
+        <div className={`lg:flex lg:gap-5 lg:justify-center lg:pb-3 sm:hidden`}>
+          <div
+            className={`lg:p-3 lg:bg-gray-50 lg:hover:bg-gray-100 lg:rounded-full lg:shadow lg:cursor-pointer lg:text-gray-700 lg:hover:text-gray-700 lg:transition lg:duration-400`}
+          >
+            <Link to={`/admin/management/${id}/profile`}>
+              <FaUserCircle />
+            </Link>
+          </div>
+          <div
+            className={`lg:p-3 lg:bg-gray-50 lg:hover:bg-gray-100 lg:rounded-full lg:shadow lg:cursor-pointer lg:text-gray-700 lg:hover:text-gray-700 lg:transition lg:duration-400`}
+          >
+            <MdNightlight />
+          </div>
+          {/** 로그아웃시 토큰삭제 */}
+          <div
+            className={`lg:p-3 lg:bg-gray-50 lg:hover:bg-gray-100 lg:rounded-full lg:shadow lg:cursor-pointer lg:text-gray-700 lg:hover:text-gray-700 lg:transition lg:duration-400`}
+          >
+            <LuLogOut />
           </div>
         </div>
         {/** 메뉴 영역 */}
@@ -87,20 +111,6 @@ export function Menu({ setToggle, toggle }: { setToggle: React.Dispatch<React.Se
               {menu.name}
             </Link>
           ))}
-        </div>
-
-        <div className={`lg:flex lg:gap-3 lg:justify-center lg:pb-3 sm:hidden`}>
-          <div
-            className={`lg:p-3 lg:bg-gray-50 lg:hover:bg-gray-100 lg:rounded-full lg:shadow lg:cursor-pointer lg:text-gray-700 lg:hover:text-gray-700 lg:transition lg:duration-400`}
-          >
-            <MdNightlight />
-          </div>
-          {/** 로그아웃시 토큰삭제 */}
-          <div
-            className={`lg:p-3 lg:bg-gray-50 lg:hover:bg-gray-100 lg:rounded-full lg:shadow lg:cursor-pointer lg:text-gray-700 lg:hover:text-gray-700 lg:transition lg:duration-400`}
-          >
-            <LuLogOut />
-          </div>
         </div>
       </div>
     </>
